9a884cb409005eaffe32dba9709c14eb599b4734
calebcochranebil <calebcochranebil33@gmail.com>
2024-01-24 16:22:28 -0700

    files

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./About.css"; // If you have specific styles for About page
+// import other necessary assets
+
+function About() {
+    return (
+        <div>
+            <h2>Our Story</h2>
+            {/* Add the rest of your about content here */}
+            <p>Content of the About page...</p>
+        </div>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import About from "./About"; // Import the About component
 import "./App.css";
+import logo from "./images/Creative Warmth.png";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        {/* Update other navigation links as needed */}
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/">{/* Home page content */}</Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
