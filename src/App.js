e15631cffd6d0146f7af48b4b4271912d982bc83
calebcochranebil <calebcochranebil33@gmail.com>
2024-01-24 12:18:36 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import "./css/style.css"; // Assuming you have global CSS
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <Features />
+                {/* Other components for different sections of your page */}
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
