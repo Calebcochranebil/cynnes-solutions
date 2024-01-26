709e956f6349da5daa35db52a4bee0604aa932f6
calebcochranebil <calebcochranebil33@gmail.com>
2024-01-26 16:07:44 -0700

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 
+import foundersImg from "../images/founders.jpg";
+
 export default function About() {
     return (
-        <div>
-            <h2>Our Story</h2>
-            {/* Add the rest of your about content here */}
-            <p>Content of the About page...</p>
+        <div className="p-6 pb-10 max-w-2xl mx-auto">
+            <h2 className="text-2xl font-semibold text-gray-800">Our Story</h2>
+            <img
+                src={foundersImg}
+                alt="Cochrane Brothers"
+                className="my-4 w-full object-cover rounded-lg shadow-md"
+            />
+            <p className="text-gray-700 text-lg">
+                The journey of our invention began with a simple observation
+                &ndash; the strenuous and time-consuming task of snow removal
+                from driveways during the harsh winters. We envisioned a more
+                efficient, sustainable, and effortless method, leading to the
+                birth of our customizable driveway heating mats.
+            </p>
+            <p className="mt-4 text-gray-700 text-lg">
+                These heating mats are designed to fit the unique dimensions of
+                each driveway, seamlessly connecting to cover the entire area.
+                The idea was to create a system that not only eliminates the
+                need for manual snow removal but also ensures safety and
+                convenience during winter months.
+            </p>
+            <p className="mt-4 text-gray-700 text-lg">
+                Our team, comprised of passionate innovators and
+                problem-solvers, dedicated countless hours to research, design,
+                and testing to bring this idea to life. The result is a product
+                that stands at the forefront of winter home maintenance,
+                revolutionizing the way snow is managed.
+            </p>
+            <p className="mt-4 text-gray-700 text-lg">
+                We are proud to present a solution that not only eases the
+                burden of winter chores but also represents our commitment to
+                innovation and excellence. Join us in embracing a smarter, more
+                efficient way to handle winter&apos;s challenges.
+            </p>
         </div>
     );
 }
