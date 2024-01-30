c9c2a6cbf5a69179a1313481f2edf2e4d47c7ff6
JarJarDinks <Cochranejacob22@gmail.com>
2024-01-29 22:10:34 -0700

    add a HowItWorks component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Features from "./components/Features";
 import Home from "./components/Home";
+import HowItWorks from "./components/HowItWorks";
 import NavBar from "./components/NavBar";
 import Quote from "./components/Quote";
 import "./App.css";
@@ -13,17 +14,18 @@ function App() {
     return (
         <Router>
             <div>
+                
                 <NavBar />
 
-                {/* Include the Features component right here */}
+                
                 <Features />
-
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/quote" element={<Quote />} />
                 </Routes>
-
+                
+                <HowItWorks />
                 <footer>
                     <p>&copy; 2024 Cynnes Solutions. All rights reserved.</p>
                 </footer>
