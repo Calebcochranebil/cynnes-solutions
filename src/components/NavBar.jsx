1727bf16ea77d45fce8a80e1a09a9313b2b96851
JarJarDinks <Cochranejacob22@gmail.com>
2024-01-31 19:21:49 -0700

    add navbar styles and responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,26 +8,26 @@ import Features from "./Features";
 export default function NavBar() {
     return (
         <>
-            <nav className="flex flex-row items-center p-4 ">
+            <nav className="flex flex-row items-center p-4 justify-between">
                 <Link to="/" className="flex-none">
                     <img
                         src={logo}
                         alt="Cynnes Solutions Logo"
-                        className="h-20 sm:h-24  "
+                        className="h-20 sm:h-24 md:pl-6 lg:pl-8 "
                     />
                 </Link>
 
-                <div className="w-3">
-                    <ul className="flex flex-row gap-4 text-md ">
-                        <li className="">
+                <div>
+                    <ul className="flex flex-row gap-4 text-md font-bold sm:text-lg md:pr-6 lg:pr-8">
+                        <li className=" hover:text-blue">
                             <Link to="/">Home</Link>
                         </li>
-                        <span>|</span>
-                        <li className="">
+                        <span className="text-orange">|</span>
+                        <li className="hover:text-blue">
                             <Link to="/quote">Quote</Link>
                         </li>
-                        <span>|</span>
-                        <li className="">
+                        <span className="text-orange">|</span>
+                        <li className="hover:text-blue">
                             <Link to="/about">About</Link>
                         </li>
                     </ul>
