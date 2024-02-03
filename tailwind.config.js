5118d3c4f3acf66d5fe7f61adc8cf886d9292dcc
JarJarDinks <Cochranejacob22@gmail.com>
2024-02-02 18:24:11 -0700

    wip, navbar and features compononent styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
             md: "768px",
             lg: "976px",
             xl: "1440px",
+            feature: "675px"
         },
         colors: {
             blue: "#39469E",
